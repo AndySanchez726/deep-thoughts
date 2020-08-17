6787005e88e1c6d118f7b10a98a7edeadea232da
Andy Sanchez <andysanchez726@gmail.com>
2020-08-16 23:52:10 -0500

    redirect profile is commented out because of an error when reloading /profile

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -28,16 +28,26 @@ const resolvers = {
             .populate('thoughts');
         },
 
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({})
-                    .select('-__v -password')
-                    .populate('thoughts')
-                    .populate('friends');
+        // me: async (parent, args) => {
+        //     const userData = await User.findOne({})
+        //     .select('-__v -password')
+        //     .populate('thoughts')
+        //     .populate('friends');
 
-                return userData;
-            }
-            throw new AuthenticationError('Not logged in');
+        //     return userData;
+        // },
+
+        me: async (parent, args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('thoughts')
+            .populate('friends');
+
+            return userData;
+        }
+
+        throw new AuthenticationError('Not logged in');
         }
     },
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,11 +29,11 @@ const typeDefs = gql `
     }
 
     type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
     }
 
     type Mutation {
